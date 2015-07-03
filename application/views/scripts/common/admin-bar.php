bb3057e08c0847f03f360a1c3dc16bca99bae63b
Franck Dupont <kyfr59@gmail.com>
2015-07-03 14:23:36 +0200

    Correcting bug on account management #2

--- application/views/scripts/common/admin-bar.php
+++ application/views/scripts/common/admin-bar.php
@@ -2,8 +2,7 @@
 
 <?php if($user = current_user()) {
 
-    if ($user->role == "super")
-    {
+    if ($user->role == 'super') {
         $links = array(
             array(
                 'label' => __('Welcome, %s', $user->name),
@@ -18,27 +17,12 @@
                 'uri' => url('/users/logout')
             )
         );
-    } else {
-        $links = array(
-            array(
-                'label' => __('Log Out'),
-                'uri' => url('/users/logout')
-            )
-        );
     }
-    
 
 } else {
     $links = array();
 }
 
-
-if ($user->role != "super")
-{
-    echo "<ul class='navigation'><li>".__('Welcome, %s', $user->name)."&nbsp;</li></ul>";
-}
-
 echo nav($links, 'public_navigation_admin_bar');
-
 ?>
 </nav>
