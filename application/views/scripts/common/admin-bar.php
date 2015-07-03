3f717368f9f445cf9c226cc48f0c10db45838e6a
Franck Dupont <kyfr59@gmail.com>
2015-07-03 14:24:32 +0200

    Correcting bug on account management #3

--- application/views/scripts/common/admin-bar.php
+++ application/views/scripts/common/admin-bar.php
@@ -2,23 +2,20 @@
 
 <?php if($user = current_user()) {
 
-    if ($user->role == 'super') {
-        $links = array(
-            array(
-                'label' => __('Welcome, %s', $user->name),
-                'uri' => admin_url('/users/edit/'.$user->id)
-            ),
-            array(
-                'label' => __('Omeka Admin'),
-                'uri' => admin_url('/')
-            ),
-            array(
-                'label' => __('Log Out'),
-                'uri' => url('/users/logout')
-            )
-        );
-    }
-
+    $links = array(
+        array(
+            'label' => __('Welcome, %s', $user->name),
+            'uri' => admin_url('/users/edit/'.$user->id)
+        ),
+        array(
+            'label' => __('Omeka Admin'),
+            'uri' => admin_url('/')
+        ),
+        array(
+            'label' => __('Log Out'),
+            'uri' => url('/users/logout')
+        )
+    );
 } else {
     $links = array();
 }
