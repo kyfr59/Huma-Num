8c8fbca590c22ee2335f91f0d01134060314c212
Franck Dupont <kyfr59@gmail.com>
2015-07-20 16:23:11 +0200

    Removing HumaNumHarvester & ExportToNakala plugins

--- plugins/HumaNumHarvester/models/OaipmhHarvester/Request/ThrottlerException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Thrown by OaipmhHarvester_Request_Throttler to block excessive requests.
- *
- * @package OaipmhHarvester
- */
-class OaipmhHarvester_Request_ThrottlerException extends Exception
-{}
